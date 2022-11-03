d53fc961e401334123b5597cc4a55b16eb88521e
Quentin PAYET <quentinpayet1203@gmail.com>
2022-11-03 13:30:09 +0400

    import de donnée test (metier plombier, depuis django réussigit add .

--- src/collab/views.py
+++ src/collab/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
-from collab.models import test, Participer
+from collab.models import test, Participer, Metier
 
 def index(request):
-    return HttpResponse(Participer.objects)
+    metierTest = Metier(codemetier = 'PLB', nommetier = 'Plombier')
+    metierTest.save()
+    return HttpResponse(Metier.objects.all())
