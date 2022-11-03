87cb0fe0695d73e2d636f2939ff9d0ec9a61cc54
Quentin PAYET <quentinpayet1203@gmail.com>
2022-11-03 15:18:25 +0400

    apres modif/test liaison BDD et PK, norm sans probleme

--- src/collab/views.py
+++ src/collab/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
-from collab.models import test, Participer, Metier
+from collab.models import test, Participer, Metier, Questions
 
 def index(request):
-    metierTest = Metier(codemetier = 'PLC', nommetier = 'Police')
+    metierTest = Metier(codemetier = 'JPRO', nommetier = "Joueur Pro")
     metierTest.save()
     return HttpResponse(Metier.objects.all())
