61493b4399f551aaebb849e887fd537c5e1fb302
Quentin PAYET <quentinpayet1203@gmail.com>
2022-11-03 17:41:48 +0400

    ajout Questions dans le model

--- src/cds/models.py
+++ src/cds/models.py
@@ -10,3 +10,18 @@ class Quiz(models.Model):
     class Meta:
         db_table = 'quiz'
         unique_together = (('noquiz', 'evaluation'),)
+
+class Questions(models.Model):
+    noquiz = models.OneToOneField('Quiz', models.DO_NOTHING, db_column='noquiz')
+    evaluation = models.BooleanField()
+    noquestion = models.CharField(max_length=50)
+    titrequestion = models.CharField(max_length=50, blank=True, null=True)
+    intitulequestion = models.CharField(max_length=50, blank=True, null=True)
+    dureequestion = models.TimeField(blank=True, null=True)
+    coefquestion = models.IntegerField(blank=True, null=True)
+    feedbackquestion = models.CharField(max_length=50, blank=True, null=True)
+    bonnereponsequestion = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'questions'
+        unique_together = (('noquiz', 'evaluation', 'noquestion'),)
