29a6e184c356237e4ae3260037e2068c123ad8cc
Quentin PAYET <quentinpayet1203@gmail.com>
2022-11-03 14:50:10 +0400

    debut app cds

--- src/cds/views.py
+++ src/cds/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from collab.models import Metier
+from django.template import loader
+
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Page CDS")
+
+    return render (request, "cds/homeCDS.html")
+
+def importQuiz(request):
+
+    #return redirect('../cds') 
+    return HttpResponse("testBouton")
