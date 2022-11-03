9b5fb2802cbb3dac6b35c6d2efd8497409ce6750
Quentin PAYET <quentinpayet1203@gmail.com>
2022-11-03 13:41:12 +0400

    requirements.txt => ajout psycopg2

--- src/collab/views.py
+++ src/collab/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 from collab.models import test, Participer, Metier
 
 def index(request):
-    metierTest = Metier(codemetier = 'PLB', nommetier = 'Plombier')
+    metierTest = Metier(codemetier = 'PLC', nommetier = 'Police')
     metierTest.save()
     return HttpResponse(Metier.objects.all())
