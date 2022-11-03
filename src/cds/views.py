bc07f68f388397ea672dd4294134ae3e75c6773b
Quentin PAYET <quentinpayet1203@gmail.com>
2022-11-03 15:42:14 +0400

    intégration code import xml to py variable fonctionnel

--- src/cds/views.py
+++ src/cds/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from collab.models import Metier
 from django.template import loader
 
+#-------------------------------------
+
+import xml.etree.ElementTree as etree
+import os
+
 # Create your views here.
 
 def index(request):
@@ -11,5 +16,45 @@ def index(request):
 
 def importQuiz(request):
 
+    doc = etree.parse('../questionnaires/questionnaires_32/32.quv') # À rendre dynamique
+    root = doc.getroot()
+
+    quest32 = {} # À rendre dynamique...
+    #Soit envoyer vers BDD plus tard dans le code avec une autre boucle
+    #Soit intégrer directement dans les boucle ci-dessous
+
+    i = 0
+    #Boucle par balise <question>, récupère ses attributs--------------------------------
+    for qst in root:
+        i += 1
+        #Numero bonne rep
+        quest32['bonneRepQ'+str(i)] = qst.get('bonne')
+        #Duree #Faire alternative si duree ou pas du style : if(qst.get('duree')) ??
+        quest32['dureeQ'+str(i)] = qst.get('duree')
+        #Coeff
+        quest32['coeffQ'+str(i)] = qst.get('coeff')
+        #Image à ajouter ?
+
+    #Récupère titre,intitulé et feedback----------------------------------------------
+    i2 = 0
+    for elt in root.findall("question"):
+        i2+=1
+        #titre
+        quest32['titreQ'+str(i2)] = elt.find('titre').text
+        #intitule
+        quest32['intituleQ'+str(i2)] = elt.find('intitule').text
+        #feedback
+        quest32['feedbackQ'+str(i2)] = elt.find('feedback').text
+
+    #Récupère les propositions de reponses-----------------------------------------------
+    iQuestion = 0
+    for listeReponse in root.findall('.//listerep'):
+        iQuestion +=1
+        iReponse = 1
+        for propRep in listeReponse:
+            #print(propRep.text,"reponse=>",iReponse,"question",iQuestion)
+            quest32['propRep'+str(iReponse)+'Q'+str(iQuestion)] = propRep.text
+            iReponse +=1
+
     #return redirect('../cds') 
-    return HttpResponse("testBouton")
+    return HttpResponse(quest32)
