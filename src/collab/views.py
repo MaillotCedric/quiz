75e51da798cff8184d8b2d02636f60c2c1d674d4
Quentin PAYET <quentinpayet1203@gmail.com>
2022-11-03 13:16:47 +0400

    création BDD mais lien entre les tables à vérifier

--- src/collab/views.py
+++ src/collab/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from collab.models import test, Participer
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse(Participer.objects)
