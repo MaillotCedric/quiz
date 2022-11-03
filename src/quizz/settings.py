c11864e1c1876129f14e2d7a6184681de0b34fa8
Quentin PAYET <quentinpayet1203@gmail.com>
2022-11-03 11:14:39 +0400

    lien PSQL en localhost, ajout app collab

--- src/quizz/settings.py
+++ src/quizz/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'quizz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quiz',
+        'USER' : 'postgres',
+        'PASSWORD' : 'azerty',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
