5174e40e30e4644c27ec4efb4c4e3b21892db9ed
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-11-07 10:08:12 +0400

    création table role

--- src/comptes/models.py
+++ src/comptes/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class Role(models.Model):
+    codeRole = models.CharField(primary_key=True, max_length=50)
+    nomRole = models.CharField(max_length=50, blank=True, null=True)
+    class Meta:
+        db_table = 'role'
+
 class Utilisateur(AbstractUser):
     pass
