b01cb02414e8c1f41c503b47a90edfa90b2016af
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-11-07 10:47:22 +0400

    table metier : création jeu d'essai

--- src/comptes/views.py
+++ src/comptes/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 # from comptes.models import Role
+# from comptes.models import Metier
 
 def index(request):
     # Role.objects.create(codeRole="admin", nomRole="administrateur")
     # Role.objects.create(codeRole="chef", nomRole="chef de secteur")
     # Role.objects.create(codeRole="collab", nomRole="collaborateur")
+    # ---------------------------------------------------------------
+    # Metier.objects.create(codeMetier="resp_rh", nomMetier="responsable ressources humaines")
+    # Metier.objects.create(codeMetier="dev_web", nomMetier="développeur web")
     return render(request, "login.html", {})
