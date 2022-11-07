18f88a06d47762eff135b151aa3e54129d13429d
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-11-07 10:37:25 +0400

    table role : création jeu d'essai

--- src/comptes/views.py
+++ src/comptes/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+# from comptes.models import Role
 
 def index(request):
+    # Role.objects.create(codeRole="admin", nomRole="administrateur")
+    # Role.objects.create(codeRole="chef", nomRole="chef de secteur")
+    # Role.objects.create(codeRole="collab", nomRole="collaborateur")
     return render(request, "login.html", {})
