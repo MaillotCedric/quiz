2d7b252b26ab5aa5b47b93ecc0705ec3616a8c16
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-11-07 11:08:42 +0400

    table secteur : création jeu d'essai

--- src/comptes/views.py
+++ src/comptes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # from comptes.models import Role
 # from comptes.models import Metier
+# from comptes.models import Secteur
 
 def index(request):
     # Role.objects.create(codeRole="admin", nomRole="administrateur")
@@ -9,4 +10,8 @@ def index(request):
     # ---------------------------------------------------------------
     # Metier.objects.create(codeMetier="resp_rh", nomMetier="responsable ressources humaines")
     # Metier.objects.create(codeMetier="dev_web", nomMetier="développeur web")
+    # ---------------------------------------------------------------
+    # Secteur.objects.create(codeSecteur="MKT", nomSecteur="marketing")
+    # Secteur.objects.create(codeSecteur="RD", nomSecteur="recherche et développement")
+    # Secteur.objects.create(codeSecteur="RH", nomSecteur="ressources humaines")
     return render(request, "login.html", {})
