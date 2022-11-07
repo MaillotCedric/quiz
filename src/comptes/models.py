71a7ada1ab787324f1667ac5db3e2f6b425d9990
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-11-07 10:51:21 +0400

    création table secteur

--- src/comptes/models.py
+++ src/comptes/models.py
@@ -13,5 +13,11 @@ class Metier(models.Model):
     class Meta:
         db_table = 'metier'
 
+class Secteur(models.Model):
+    codeSecteur = models.CharField(primary_key=True, max_length=50)
+    nomSecteur = models.CharField(max_length=50, blank=True, null=True)
+    class Meta:
+        db_table = 'secteur'
+
 class Utilisateur(AbstractUser):
     pass
