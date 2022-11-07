28212211bf1d57203fe7682830c2ba7e7e8de5d4
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-11-07 11:24:15 +0400

    table utilisateur : ajout colonnes
    ajout colonne matricule
    ajout colonne codeSecteur
    ajout colonne codeMetier
    ajout colonne codeRole

--- src/comptes/models.py
+++ src/comptes/models.py
@@ -20,4 +20,11 @@ class Secteur(models.Model):
         db_table = 'secteur'
 
 class Utilisateur(AbstractUser):
-    pass
+    matricule = models.CharField(max_length=50, null=False, default="AAAA")
+    codeSecteur = models.ForeignKey(Secteur, models.DO_NOTHING, db_column='codeSecteur', null=False, default="MKT")
+    codeMetier = models.ForeignKey(Metier, models.DO_NOTHING, db_column='codeMetier', null=False, default="resp_rh")
+    codeRole = models.ForeignKey(Role, models.DO_NOTHING, db_column='codeRole', null=False, default="admin")
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["matricule"], name='unique matricule')
+        ]
