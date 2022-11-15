23dfb7c6aaa875ca2bb95699151a2cf8fb3bd39f
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-11-15 09:47:38 +0400

    page admin django : ajout tables
    . Role
    . Metier
    . Secteur
    . Utilisateur

--- src/comptes/admin.py
+++ src/comptes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from comptes.models import Role, Metier, Secteur, Utilisateur
 
 # Register your models here.
+
+admin.site.register(Role)
+admin.site.register(Metier)
+admin.site.register(Secteur)
+admin.site.register(Utilisateur)
